3c0c8b821e01f4b032ae1a41280c90878e0a3153
etnevelpriv <leventerastovits@gmail.com>
2025-11-21 16:05:48 +0100

    Add project files.

---
+++ tanulosProjektMatematika/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tanulosProjektMatematika
+{
+    class Main
+    {
+        String kerdes;
+        String valaszA;
+        String valaszB;
+        String valaszC;
+        String valaszD;
+        String valaszHelyes;
+
+        public Main(String kerdes, String valaszA, String valaszB, String valaszC, String valaszD, String valaszHelyes )
+        {
+            this.kerdes = kerdes;
+            this.valaszA = valaszA;
+            this.valaszB = valaszB;
+            this.valaszC = valaszC;
+            this.valaszD = valaszD;
+            this.valaszHelyes = valaszHelyes;
+        }
+        
+        public String getKerdes() { return kerdes; }
+        public String getValaszA() { return valaszA; }
+        public String getValaszB() { return valaszB; }
+        public String getValaszC() { return valaszC; }
+        public String getValaszD() { return valaszD; }
+        public String getValaszHelyes() { return valaszHelyes; }
+
+    }
+}
---
+++ tanulosProjektMatematika/fajlBeolvasas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tanulosProjektMatematika
+{
+    class fajlBeolvasas
+    {
+        public List<Main> kerdesek = new List<Main>();
+
+        public fajlBeolvasas()
+        {
+            using (StreamReader sr = new StreamReader("matekKerdesek.txt"))
+            {
+                string sor = sr.ReadLine();
+                while (sor != null)
+                {
+                    String[] mezok = sor.Split(';');
+                    Main kerdes = new Main(mezok[0], mezok[1], mezok[2], mezok[3], mezok[4], mezok[5]);
+                    kerdesek.Add(kerdes);
+                    sor = sr.ReadLine();
+                }
+            }
+        }
+    }
+}
